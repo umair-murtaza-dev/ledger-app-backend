726881e9ebee4696d55f936114b82f331b21cb4e
mumair <mumair-ext@unifonic.com>
2022-12-29 13:25:53 +0500

    rm

--- config/initializers/zipkin.rb
+++
@@ -1,7 +0,0 @@
-# require 'zipkin-tracer'
-# Rails.application.config.zipkin_tracer = {
-#   service_name: 'ledger',
-#   json_api_host: 'https://zipkin.shared.dev.unifonic.com',
-#   sampled_as_boolean: false,
-#   sample_rate: 0.1 # 0.0 to 1.0, where 1.0 => 100% of requests
-# }
