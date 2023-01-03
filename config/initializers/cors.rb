18b416d6054cb6f786ccc6e12d31e9db9ca0e76b
mumair <mumair-ext@unifonic.com>
2023-01-04 01:29:15 +0500

    update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,7 +10,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins '*'
 
     resource '*',
-      headers: 'x-authorization-token',
+      headers: :any,
       expose: ['Authorization'],
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: false
