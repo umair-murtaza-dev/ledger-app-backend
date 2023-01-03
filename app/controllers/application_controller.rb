cc2b80dbad92bc4e0757a591542c9e913dfd41b4
mumair <mumair-ext@unifonic.com>
2023-01-03 10:48:41 +0500

    update cords

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::API
+  before_action :cors_preflight_check
+after_action :cors_set_access_control_headers
+
+# For all responses in this controller, return the CORS access control headers.
+
+def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With auth_token X-CSRF-Token Authorization, authorization}.join(',')
+    headers['Access-Control-Max-Age'] = "1728000"
+end
+
+def cors_preflight_check
+  if request.method == "OPTIONS"
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With auth_token X-CSRF-Token Authorization authorization}.join(',')
+    headers['Access-Control-Max-Age'] = '1728000'
+    render :text => '', :content_type => 'text/plain'
+  end
+end
 end
